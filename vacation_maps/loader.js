e0bc4a4bdb006639192f42c155caa59c419293ec
Simone Cesano <simonecesano@libero.it>
2019-06-08 15:33:15 +0200

    Added handlebars

--- vacation_maps/loader.js
+++ vacation_maps/loader.js
@@ -1,6 +1,22 @@
 var token = "pk.eyJ1Ijoic2ltb25lY2VzYW5vIiwiYSI6ImNqdDR0eHY0cDA2cm80M255dmR6OHk5N3kifQ.GgSBPOLgJkbgvtOYQebzpA"
 var mymap;
 
+var template = `
+{{#if href}}
+<a href="{{href}}">{{ name }}</a>
+{{else}}
+{{ name }}
+{{/if}}<br />
+{{#if street}}
+{{ street }}
+{{/if}}
+{{#if img}}
+<img src="{{img}}">
+{{/if}}`
+template = Handlebars.compile(template);
+
+var markers = [];
+
 var attr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> ' +
     'contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
     'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
@@ -14,19 +30,24 @@ var parseCoord = function(s) {
 var refresh = function(json) {
     $.get(json, function(d){
 	d = d.filter(e => {
-	    return true
+	    return !(e.lon === 'N/A') && !(e.lat === 'N/A') && e.lon && e.lat
 	}).map(e => {
-	    // e.longitude = parseCoord(e.lon)
-	    // e.latitude = parseCoord(e.lat)
 	    e.longitude = e.lon
 	    e.latitude  = e.lat
 	    return e
 	});
 	
 	var c = d[0]
+	if (!mymap) {
+	    mymap = L.map('mapid')
+	} else {
+	    // console.log(mymap.getPane('markerPane').remove())
+	    // console.log(mymap.getPane('markerPane').remove())
+	}
 
-	console.log(mymap);
-	if (!mymap) { mymap = L.map('mapid') }
+
+	
+
 	mymap.setView([c.latitude, c.longitude ], 13)
 	
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -35,17 +56,22 @@ var refresh = function(json) {
 	    id: 'mapbox.streets',
 	    accessToken: token
 	}).addTo(mymap)
-	
+
+	markers.forEach(m => { m.remove() })
+
 	d.forEach(e => {
 	    try {
 		var marker = L.marker([e.latitude, e.longitude]).addTo(mymap);
-		// var img = e.img.match(/N\/A/) ? '' : `<br /><img src="${e.img}">`
-		marker.bindPopup(`${e.name}<br />${e.street}`)
+		markers.push(marker);
+		marker.bindPopup(template(e))
 	    } catch (err){
 		console.log(err);
-		console.log(JSON.stringify(e));
+		// console.log(JSON.stringify(e));
 	    }
 	})
+
+	var group = new L.featureGroup(markers);
+	    mymap.fitBounds(group.getBounds());
     })
 }
 
